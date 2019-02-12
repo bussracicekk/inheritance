fbb0b2b1183d38df441ea491724441054e36b0ce
bussracicekk <37375743+bussracicekk@users.noreply.github.com>
2019-02-13 02:32:50 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,6 @@
 
 //This program creates a output_files folder and saves all of output files to it.
 
-package assignment2;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
